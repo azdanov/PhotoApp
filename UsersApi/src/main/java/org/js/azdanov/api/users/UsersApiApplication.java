ce548696e22c2bd6bf462b11bafcffd0bdb815a9
Anton Ždanov <azdanov@pm.me>
2021-04-26 21:58:01 +0300

    feat: add users service

---
+++ UsersApi/src/main/java/org/js/azdanov/api/users/UsersApiApplication.java
@@ -0,0 +1,15 @@
+package org.js.azdanov.api.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class UsersApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersApiApplication.class, args);
+    }
+
+}
