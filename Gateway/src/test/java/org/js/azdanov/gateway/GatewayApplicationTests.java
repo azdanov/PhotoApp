86f9098ae74bf4892d19af902c89707eca25324f
Anton Ždanov <azdanov@pm.me>
2021-04-27 19:18:03 +0300

    feat: add discovery service
    - lower version of spring due to a bug in rc1 (random.value not working)

---
+++ Gateway/src/test/java/org/js/azdanov/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.js.azdanov.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
