1995ad2061f0f7ef8c8becafbf6779547596e4b7
Anton Ždanov <azdanov@pm.me>
2021-05-01 13:36:37 +0300

    feat: add configuration service

---
+++ Configuration/src/test/java/org/js/azdanov/configuration/ConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.js.azdanov.configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
