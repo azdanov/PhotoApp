90d2c27a31018357f7844d11f08d8a888bffe6f7
Anton Ždanov <azdanov@pm.me>
2021-05-06 22:12:29 +0300

    feat: use Transactional readOnly for find methods

--- UsersApi/src/main/java/org/js/azdanov/api/users/data/UsersRepository.java
+++ UsersApi/src/main/java/org/js/azdanov/api/users/data/UsersRepository.java
@@ -1,12 +1,13 @@
 package org.js.azdanov.api.users.data;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
-@Repository
+@Transactional(readOnly = true)
 public interface UsersRepository extends CrudRepository<UserEntity, Long> {
     Optional<UserEntity> findByEmail(String email);
+
     Optional<UserEntity> findByUserId(String userId);
 }
